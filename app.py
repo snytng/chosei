25068165f9fb86e787762e16e184b8c435a97819
Shinya TANIGUCHI <taniguchi.shinya@exc.epson.co.jp>
2022-12-17 22:14:01 +0900

    app変数を追加、runにserver=gnunicornを追加

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 ﻿# coding: utf-8
-from bottle import route, run, request, response, redirect, static_file, template
+from bottle import route, run, request, response, redirect, static_file, template, default_app
 import random, string
 import base64
 
 @route('/')
-def root():
-    return redirect("/index.html")
-    
+def hello():
+    return "<h1>Hello world!</h1>"
+
 @route('/index.html')
 def index():
     return template("index")
 
 # -----
 if __name__ == "__main__":
-    run(host='0.0.0.0', port=80, debug=True, reloader=True)
+    run(host='0.0.0.0', port=80, server="gnuicorn", workers=4, debug=True, reloader=True)
+
+app = default_app()
